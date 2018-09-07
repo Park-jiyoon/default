bd0b09352a15d15e287eaa5199e984cd39e41f62
Jiyoon <bbpgy@sju.ac.kr>
2018-09-07 15:31:36 +0900

    Initial commit

---
+++ app/src/main/java/org/androidtown/sksunny/MainActivity.java
@@ -0,0 +1,38 @@
+package org.androidtown.sksunny;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.app.Activity;
+import android.graphics.Rect;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final int REQUEST_CODE_MENU=101;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        SlidingView sv = new SlidingView(this);
+        View v1 = View.inflate(this, R.layout.t1, null);
+        View v2 = View.inflate(this, R.layout.t2, null);
+        sv.addView(v1);
+        sv.addView(v2);
+        setContentView(R.layout.activity_main);
+    }
+    public void onButton1Clicked(View v){
+        Intent intent = new Intent(getApplicationContext(), MenuActivity.class);
+        startActivityForResult(intent,REQUEST_CODE_MENU);
+    }
+}
